74560e47c2c438a843e1eecce75eb83a2461874b
mskim-poly <mskim@polyglotsoftology.com>
2024-08-06 14:00:45 +0900

    commit message

---
+++ src/git/second_test.java
@@ -0,0 +1,7 @@
+package git;
+
+public class second_test {
+	public static void main(String args[]) {
+		System.out.println("second push");
+	}
+}
---
+++ src/git/test.java
@@ -0,0 +1,12 @@
+
+package git;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("test source");
+		System.out.println("new Push Head");
+		
+		
+		
+	}
+}
